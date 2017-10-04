583db7c35207f4b80c2ecd5335e4c76095072ebf
belousAleksandr <belous803@gmail.com>
2017-10-04 18:07:12 +0300

    fix commands list

--- app_start.sh
+++ app_start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -u $MYSQL_USER -p$MYSQL_PASSWORD
+
+/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
+/usr/bin/php /unison/admin/cli/install_database.php --agree-license --adminuser=$MOODLE_USERNAME --adminpass=$MOODLE_PASSWORD --adminemail=$MOODLE_EMAIL --fullname=MOODLE_SITENAME --shortname=$MOODLE_SITENAME_SHORT
 set -e
 
 source /etc/apache2/envvars
@@ -7,5 +11,3 @@ rm -f $APACHE_PID_FILE
 
 exec /usr/sbin/apache2 -D FOREGROUND
 
-/usr/bin/php /unison/admin/cli/install_database.php --agree-license --adminuser=$MOODLE_USERNAME --adminpass=$MOODLE_PASSWORD --adminemail=$MOODLE_EMAIL --fullname=MOODLE_SITENAME --shortname=$MOODLE_SITENAME_SHORT
-/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
