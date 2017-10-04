1d09c7c4c487c33c880b9b7a2ea19bb236ec70de
belousAleksandr <belous803@gmail.com>
2017-10-04 17:07:19 +0300

    update app_start.sh

--- app_start.sh
+++ app_start.sh
@@ -6,3 +6,6 @@ source /etc/apache2/envvars
 rm -f $APACHE_PID_FILE
 
 exec /usr/sbin/apache2 -D FOREGROUND
+
+/usr/bin/php /unison/admin/cli/install_database.php --agree-license --adminuser=admin --adminpass='test!QAZ2wsx' --adminemail=test@example.com --fullname=test --shortname=test
+/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
