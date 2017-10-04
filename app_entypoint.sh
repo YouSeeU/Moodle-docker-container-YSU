8b08d953055c14d7f9446ab7ec088b159783e586
belousAleksandr <belous803@gmail.com>
2017-10-04 18:12:57 +0300

    add entrypoint.sh

---
+++ app_entypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -u $MYSQL_USER -p$MYSQL_PASSWORD
+
+/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
+/usr/bin/php /unison/admin/cli/install_database.php --agree-license --adminuser=$MOODLE_USERNAME --adminpass=$MOODLE_PASSWORD --adminemail=$MOODLE_EMAIL --fullname=$MOODLE_SITENAME --shortname=$MOODLE_SITENAME_SHORT
--- app_start.sh
+++ app_start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -u $MYSQL_USER -p$MYSQL_PASSWORD
-
-/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
-/usr/bin/php /unison/admin/cli/install_database.php --agree-license --adminuser=$MOODLE_USERNAME --adminpass=$MOODLE_PASSWORD --adminemail=$MOODLE_EMAIL --fullname=MOODLE_SITENAME --shortname=$MOODLE_SITENAME_SHORT
 set -e
 
 source /etc/apache2/envvars
