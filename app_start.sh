5c25778eba8a30db3039ff2572be504029a30c39
belousAleksandr <belous803@gmail.com>
2017-10-11 18:57:50 +0300

    CSD-5493 Init project

---
+++ app_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD
+
+/usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME < /usr/local/bin/moodle_db.sql
+set -e
+
+source /etc/apache2/envvars
+
+rm -f $APACHE_PID_FILE
+
+exec /usr/sbin/apache2 -D FOREGROUND
+
---
+++ moodle-config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = getenv('MYSQL_HOST');
+$CFG->dbname    = getenv('MYSQL_DATABASE_NAME');
+$CFG->dbuser    = getenv('MYSQL_USER');
+$CFG->dbpass    = getenv('MYSQL_PASSWORD');
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+    'dbpersist' => 0,
+    'dbport' => getenv('MYSQL_PORT_NUMBER'),
+    'dbsocket' => '',
+    'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://moodle.loc:444';
+$CFG->dataroot  = '/home/webapp/moodledata';
+$CFG->admin     = getenv('MOODLE_USERNAME');
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
