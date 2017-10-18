7596efe3fada3a2054bc088b1eadbf2d29440417
belousAleksandr <belous_aleksandr@rambler.ru>
2017-10-18 14:28:44 +0300

    CSD-5493 Add change password for users after upload database

--- app_start.sh
+++ app_start.sh
@@ -3,6 +3,9 @@ echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -h $MYSQL_HOS
 sed -i "s/\${YSU_HOST}/$YSU_HOST/g" /usr/local/bin/moodle_db.sql
 /usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME < /usr/local/bin/moodle_db.sql
+/usr/bin/php /unison/admin/cli/reset_password.php --username=admin --password=$MOODLE_PASSWORD --ignore-password-policy
+/usr/bin/php /unison/admin/cli/reset_password.php --username=student --password=$MOODLE_PASSWORD --ignore-password-policy
+/usr/bin/php /unison/admin/cli/reset_password.php --username=contentdeveloper  --password=$MOODLE_PASSWORD --ignore-password-policy
 set -e
 
 source /etc/apache2/envvars
