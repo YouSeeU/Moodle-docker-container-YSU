f8ed759819988675229b298abec2c1632ae3fb91
belousAleksandr <belous_aleksandr@rambler.ru>
2017-10-18 11:52:59 +0300

    CSD-5493 Fix variable name

--- app_start.sh
+++ app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;" | mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD
-echo "SET @doamin = $YSU_HOST;" | mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD
+echo "SET @domain = $YSU_HOST;" | mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD
 
 /usr/bin/php /unison/admin/cli/mysql_collation.php --collation=utf8mb4_unicode_ci
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME < /usr/local/bin/moodle_db.sql
