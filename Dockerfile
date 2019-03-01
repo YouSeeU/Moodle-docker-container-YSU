3aea63e0eec6f08ec5da4a114e4e7a02a4245e91
leon <dinoel.zik@gmail.com>
2019-03-01 19:39:28 +0200

    YSU-2441 Fix building moodle image

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN apt-get update && \
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 # download Moodle
-RUN git clone --depth=1 -b v3.3.3 git://git.moodle.org/moodle.git /var/www/html/
+RUN git clone --depth=1 -b v3.2.9 git://git.moodle.org/moodle.git /var/www/html/
 
 # download Moosh
 RUN git clone --depth=1 -b 0.27 git://github.com/tmuras/moosh.git /var/www/html/moosh \
@@ -21,8 +21,6 @@ RUN git clone --depth=1 -b 0.27 git://github.com/tmuras/moosh.git /var/www/html/
 COPY apache /etc/apache2/
 RUN a2enmod ssl
 
-COPY moodle-config.php /var/www/html/config.php
-COPY db/moodle_db.sql /tmp
 COPY app_start.sh /usr/local/bin/
 
 RUN chmod +x /usr/local/bin/app_start.sh
--- app_start.sh
+++ app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Waiting for Mysql to be available...'
+echo 'Waiting for Mysql server to be available...'
 maxTries=10; try=0
 while [ "$try" -lt "$maxTries" ] && ! mysql -h "$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e 'DO 1'; do
     sleep 10
@@ -15,9 +15,12 @@ fi
 MOODLE_CLI_DIR="/var/www/html/admin/cli"
 
 if [[ ! $(mysql -h "$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '"$MYSQL_DATABASE_NAME"'") ]] ; then
-    echo "Database not exist. Try create"
-    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;"
-    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE_NAME"< /tmp/moodle_db.sql
+    echo "Database does not exist. Trying to create ..."
+
+    php "$MOODLE_CLI_DIR/install.php"  --wwwroot="$MOODLE_URL" --dbtype="$DATABASE_TYPE" --dbhost="$MYSQL_HOST" --dbname="$MYSQL_DATABASE_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --fullname="$MOODLE_SITENAME" --shortname="$MOODLE_SITENAME_SHORT" --adminuser="$MOODLE_USERNAME" --adminpass="$MOODLE_PASSWORD" --agree-license=1 --non-interactive
+
+    chmod a+r /var/www/html/config.php
+
     php "$MOODLE_CLI_DIR/mysql_collation.php" --collation=utf8mb4_unicode_ci > /dev/null
     # creating educators
     for i in `seq 1 3`; do
